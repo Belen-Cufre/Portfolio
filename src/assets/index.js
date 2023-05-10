331eac372ab7b0abce9ae69d80870f6c741e7e73
Belen-Cufre <belen_cufre@hotmail.com>
2023-05-09 21:29:07 -0300

    Portfolio: Techs OK

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,29 +3,26 @@ import backend from "./backend.png";
 import creator from "./creator.png";
 import mobile from "./translation.png";
 import web from "./web.png";
-import github from "./github.png";
+import github from "./tech/github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
 import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
+import memoq from "./tech/memoq.png";
+import postgresql from "./tech/postgresql.png";
+import sequelize from "./tech/sequelize.png";
+import trados from "./tech/trados.png";
+import wf from "./tech/wf.jpg";
 import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -39,18 +36,18 @@ export {
   github,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
+  css, 
   html,
   javascript,
-  mongodb,
   nodejs,
   reactjs,
+  memoq,
+  postgresql,
+  sequelize,
+  trados,
+  wf,
   redux,
   tailwind,
-  typescript,
   threejs,
   meta,
   shopify,
